f09a37be8eb62b561570970c0ad793d2478832ce
Dusan Susic <dusan.susic@gmail.com>
2021-05-31 11:07:54 +0200

    Switch to bulk upload

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ from io import BytesIO
 import re
 import os
 import json
+import time
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from elasticsearch import helpers
+from botocore.client import Config
 
 
 aws_region = os.environ.get('REGION')
@@ -19,29 +23,15 @@ es_index_prefix = os.environ.get('ES_INDEX_PREFIX', "alb-logs")
 es_index_doc_type = os.environ.get('ES_INDEX_DOC_TYPE')
 es_endpoint = os.environ.get('ES_ENDPOINT')
 es_kibana_endpoint = os.environ.get('ES_KIBANA_ENDPOINT')
+es_bulk_chunk_size = os.environ.get('ES_BULK_CHUNK_SIZE', "500")
 
 # Initialize Logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
-def index_doc_element(es_url, awsauth, doc_data):
-    try:
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-
-        resp = requests.post(es_url, auth=awsauth, headers=headers, json=doc_data)
-
-        if resp.status_code == 201:
-            logger.info('INFO: Successfully inserted element into ES')
-        else:
-            logger.error('FAILURE: Unable to index element')
-    except Exception as e:
-        logger.error('ERROR: {0}'.format(str(e)))
-        print(e)
-
-
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
+    config = Config(connect_timeout=5, retries={'max_attempts': 0})
+    s3 = boto3.client('s3', region_name=aws_region, config=config)
     credentials = boto3.Session().get_credentials()
     awsauth = AWS4Auth(credentials.access_key,
                        credentials.secret_key,
@@ -63,7 +53,7 @@ def lambda_handler(event, context):
     except Exception as e:
         logger.error('ERROR: {0}'.format(str(e)))
         logger.error(
-            'ERROR: Unable able to GET object:{0} from S3 Bucket:{1}. Verify object exists.'.format(key, bucket))
+        'ERROR: Unable able to GET object:{0} from S3 Bucket:{1}. Verify object exists.'.format(key, bucket))
 
     if (key.endswith('.gz')) or (key.endswith('.tar.gz')):
         mycontentzip = gzip.GzipFile(
@@ -85,10 +75,16 @@ def lambda_handler(event, context):
                  str(datetime.date.today().month) + '-' + \
                  str(datetime.date.today().day)
 
-    es_url = es_endpoint + '/' + index_name + '/' + es_index_doc_type
+    es = Elasticsearch(
+        hosts=[{'host': es_endpoint, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection
+    )
 
     doc_data = {}
-
+    cnt = 0
     fields = [
         "type",
         "timestamp",
@@ -123,25 +119,25 @@ def lambda_handler(event, context):
         "classification",
         "classification_reason"
     ]
-
     regex = r"([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*):([0-9]*) ([^ ]*)[:-]([0-9]*) ([-.0-9]*) ([-.0-9]*) ([-.0-9]*) (|[" \
             r"-0-9]*) (-|[-0-9]*) ([-0-9]*) ([-0-9]*) \"([^ ]*) ([^ ]*) (- |[^ ]*)\" \"([^\"]*)\" ([A-Z0-9-]+) ([" \
             r"A-Za-z0-9.-]*) ([^ ]*) \"([^\"]*)\" \"([^\"]*)\" \"([^\"]*)\" ([-.0-9]*) ([^ ]*) \"([^\"]*)\" (.*) (.*) " \
             r"(.*) (.*) (.*) "
+    actions = []
 
     for line in lines:
         matches = re.search(regex, line)
-
         if matches:
+            cnt += 1
             for i, field in enumerate(fields):
-                end = ", " if i < len(fields) - 1 else "\n"
-                print("%s:\"%s\"" % (field, matches.group(i + 1)), end=end)
-
                 doc_data[field] = matches.group(i + 1)
+            actions.append(doc_data.copy())
+        if cnt % int(es_bulk_chunk_size) == 0:
+            print(helpers.bulk(es, actions, index=index_name, doc_type=es_index_doc_type, chunk_size=es_bulk_chunk_size))
+            actions = []
+    print(helpers.bulk(es, actions, index=index_name, doc_type=es_index_doc_type, chunk_size=es_bulk_chunk_size))
 
-            index_doc_element(es_url, awsauth, doc_data)
     logger.info('File processing complete. Check logs at %s' % es_kibana_endpoint)
-
 
 if __name__ == '__main__':
     lambda_handler(None, None)
